f978f9a4f4c845a3ac77aafcea4d97a53d0dcaba
brethepanther12 <bmoleus.nn@gmail.com>
2025-01-29 18:16:26 -0500

    Addded varibles to the Test_Method. Created three objects of HelloWorld, three string variables and Asserted all three.

--- FixedGroupProjectTravBre/FixedGroupProjectTravBre.cpp
+++ FixedGroupProjectTravBre/FixedGroupProjectTravBre.cpp
@@ -11,10 +11,17 @@ namespace FixedGroupProjectTravBre
 		
 		TEST_METHOD(ShowMessage)
 		{
-			HelloWorld objectTest1, objectTest2;
-			std::string test1, test2;
+			HelloWorld objectTest1, objectTest2,objectTest3;
+			std::string test1, test2, test3;
 
 			std::string Hello_ = "Hello World!";
+
+			test1 = objectTest1.ShowMessage(Hello_);
+			test2 = objectTest2.ShowMessage("Goodbye World!");
+			test3 = objectTest3.ShowMessage(Hello_);
+
+			Assert::AreEqual(test1, test2);
+			Assert::AreEqual(test1, test3);
 		}
 	};
 }
