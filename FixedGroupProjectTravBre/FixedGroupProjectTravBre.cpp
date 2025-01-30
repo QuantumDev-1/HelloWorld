0d66f8bb6d0248c1e8a7861f53577765224702b7
QuantumDev-1 <travis.byers96@gmail.com>
2025-01-29 19:59:16 -0600

    Fixed unresolved external error by adding both .h and .cpp files. Optimized code by cutting variables and updating ShowMessage to simply return "Hello World!". Test passes.

--- FixedGroupProjectTravBre/FixedGroupProjectTravBre.cpp
+++ FixedGroupProjectTravBre/FixedGroupProjectTravBre.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "C:\Users\Bre\source\repos\HelloWorld\HelloWorld\HelloWorld.h"
+#include "..\HelloWorld\HelloWorld.h"
+#include "..\HelloWorld\HelloWorld.cpp"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace FixedGroupProjectTravBre
@@ -11,17 +12,12 @@ namespace FixedGroupProjectTravBre
 		
 		TEST_METHOD(ShowMessage)
 		{
-			HelloWorld objectTest1, objectTest2,objectTest3;
-			std::string test1, test2, test3;
+			HelloWorld objectTest1;
+			std::string test1 = objectTest1.ShowMessage();
 
 			std::string Hello_ = "Hello World!";
 
-			test1 = objectTest1.ShowMessage(Hello_);
-			test2 = objectTest2.ShowMessage("Goodbye World!");
-			test3 = objectTest3.ShowMessage(Hello_);
-
-			Assert::AreEqual(test1, test2);
-			Assert::AreEqual(test1, test3);
+			Assert::AreEqual(test1, Hello_);
 		}
 	};
 }
